33482b8f76b6279001243ba15c225a2421a660cc
anquetil <nicolas.anquetil@inria.fr>
2023-09-26 15:10:53 +0200

    Introduced SXML_CONCAT function to replace all others

--- trunk/incl/sxml.h
+++ trunk/incl/sxml.h
@@ -20,6 +20,7 @@
 char WHAT_XMLBUILD[] = "@(#)SYNTAX - $Id: sxml.h 3546 2023-08-26 07:53:18Z garavel $";
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <assert.h>
 #include <math.h> /* for log10l() ; add -lm option on Linux */
 
@@ -162,239 +163,35 @@ SXML_TYPE_LIST SXML_TT (
 	return SXML_LL (SXML_T (T1), SXML_T (T2));
 }
 
-/* ------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------
+ * Concatenate JSON_TYPE_TEXTs and JSON_TYPE_LISTs according to parameter 'format'
+*/
 
-SXML_TYPE_LIST SXML_LLL (
-		SXML_TYPE_LIST L1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3)
-{
-	return SXML_LL (SXML_LL (L1, L2), L3);
+SXML_TYPE_LIST SXML_CONCAT (char *format, ...) {
+	va_list args;
+	int nb_args;
+	SXML_TYPE_LIST L;
+	int i;
+
+	nb_args = strlen(format);
+	va_start (args, format);
+
+	if (*format == 'T') {
+		L = SXML_T( va_arg(args, SXML_TYPE_TEXT));
+	}
+	else {
+		L = SXML_L( va_arg(args, SXML_TYPE_LIST));
+	}
+
+	for (i=1, format++; i<nb_args; i++, format++) {
+		if (*format == 'T') {
+			L = SXML_LL (L, SXML_T( va_arg(args, SXML_TYPE_TEXT)));
+		}
+		else {
+			L = SXML_LL (L, SXML_L( va_arg(args, SXML_TYPE_LIST)));
+		}
+	}
+	va_end (args);
+
+	return L;
 }
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_LTL (
-		SXML_TYPE_LIST L1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_LIST L3)
-{
-	return SXML_LLL (L1, SXML_T (T2), L3);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TLL (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3)
-{
-	return SXML_LLL (SXML_T (T1), L2, L3);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TLT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_TEXT T3)
-{
-	return SXML_LLL (SXML_T (T1), L2, SXML_T (T3));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTL (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_LIST L3)
-{
-	return SXML_LLL (SXML_T (T1), SXML_T (T2), L3);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-	 	SXML_TYPE_TEXT T3)
-{
-	return SXML_LLL (SXML_T (T1), SXML_T (T2), SXML_T (T3));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_LLLL (
-		SXML_TYPE_LIST L1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3,
-		SXML_TYPE_LIST L4)
-{
-	return SXML_LL (SXML_LLL (L1, L2, L3), L4);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_LTTT (
-		SXML_TYPE_LIST L1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3,
-		SXML_TYPE_TEXT T4)
-{
-	return SXML_LL (L1, SXML_TTT (T2, T3, T4));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TLLT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3,
-		SXML_TYPE_TEXT T4)
-{
-	return SXML_LLLL (SXML_T (T1), L2, L3, SXML_T (T4));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTL (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_LIST L4)
-{
-	return SXML_LL (SXML_TTT (T1, T2, T3), L4);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-	 	SXML_TYPE_TEXT T3,
-		SXML_TYPE_TEXT T4)
-{
-	return SXML_LLLL (SXML_T (T1), SXML_T (T2), SXML_T (T3), SXML_T (T4));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_LLLLL (
-		SXML_TYPE_LIST L1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3,
-		SXML_TYPE_LIST L4,
-		SXML_TYPE_LIST L5)
-{
-	return SXML_LL (SXML_LLLL (L1, L2, L3, L4), L5);
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TLLLT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_LIST L2,
-		SXML_TYPE_LIST L3,
-		SXML_TYPE_LIST L4,
-		SXML_TYPE_TEXT T5)
-{
-	return SXML_LLLLL (SXML_T (T1), L2, L3, L4, SXML_T (T5));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3,
-		SXML_TYPE_TEXT T4,
-		SXML_TYPE_TEXT T5)
-{
-	return SXML_LL (SXML_TTT (T1, T2, T3), SXML_TT (T4, T5));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTLT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_LIST L4,
-		SXML_TYPE_TEXT T5)
-{
-	return SXML_LLL (SXML_TTT (T1, T2, T3), L4, SXML_T (T5));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_TEXT T4,
-		SXML_TYPE_TEXT T5,
-		SXML_TYPE_TEXT T6)
-{
-	return SXML_LL (SXML_TTT (T1, T2, T3), SXML_TTT (T4, T5, T6));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTLTLT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_LIST L4,
-		SXML_TYPE_TEXT T5,
-		SXML_TYPE_LIST L6,
-		SXML_TYPE_TEXT T7)
-{
-	return SXML_LLL (SXML_TTTLT (T1, T2, T3, L4, T5), L6, SXML_T (T7));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTTTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_TEXT T4,
-		SXML_TYPE_TEXT T5,
-		SXML_TYPE_TEXT T6,
-		SXML_TYPE_TEXT T7)
-{
-	return SXML_LL (SXML_TTTT (T1, T2, T3, T4), SXML_TTT (T5, T6, T7));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTTTTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_TEXT T4,
-		SXML_TYPE_TEXT T5,
-		SXML_TYPE_TEXT T6,
-		SXML_TYPE_TEXT T7,
-		SXML_TYPE_TEXT T8)
-{
-	return SXML_LL (SXML_TTTT (T1, T2, T3, T4), SXML_TTTT (T5, T6, T7, T8));
-}
-
-/* ------------------------------------------------------------------------- */
-
-SXML_TYPE_LIST SXML_TTTTTTTTT (
-		SXML_TYPE_TEXT T1,
-		SXML_TYPE_TEXT T2,
-		SXML_TYPE_TEXT T3, 
-		SXML_TYPE_TEXT T4,
-		SXML_TYPE_TEXT T5,
-		SXML_TYPE_TEXT T6,
-		SXML_TYPE_TEXT T7,
-		SXML_TYPE_TEXT T8,
-		SXML_TYPE_TEXT T9)
-{
-	return SXML_LL (SXML_TTTT (T1, T2, T3, T4), SXML_TTTTT (T5, T6, T7, T8, T9));
-}
-
-/* ------------------------------------------------------------------------- */
