8966d53c238c242e3855919a8411213d7a719b47
anquetil <nicolas.anquetil@inria.fr>
2023-10-03 19:49:05 +0200

    introduced JSON key/value output (value is a text)

--- trunk/examples/j77/sjson.h
+++ trunk/examples/j77/sjson.h
@@ -1,5 +1,30 @@
 
+/* -------------------------------------------------------------------------
+ * outputs a JSON array:
+ *   "[" LIST "]"
+ */
+
 SXML_TYPE_LIST JSON_ARRAY (SXML_TYPE_LIST L)
 {
   return SXML_TLT( "\n[\n", L, "\n]");
+}
+
+/* -------------------------------------------------------------------------
+ * outputs a JSON key/value pair where value is a text:
+ *   quoted(K) ":" quoted(value)
+ */
+
+SXML_TYPE_LIST JSON_KT (SXML_TYPE_TEXT K, SXML_TYPE_TEXT V)
+{
+  return SXML_TTTTT( "\"", K, "\":\"", V, "\"\n");
+}
+
+/* -------------------------------------------------------------------------
+ * same as preceeding with a comma at the end:
+ *   quoted(K) ":" quoted(value) ","
+ */
+
+SXML_TYPE_LIST JSON_KTV (SXML_TYPE_TEXT K, SXML_TYPE_TEXT V)
+{
+  return SXML_TTTTT( "\"", K, "\":\"", V, "\",\n");
 }
