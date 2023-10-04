970a6282b2a5164a7b457ab6708ec6900fcee8f6
anquetil <nicolas.anquetil@inria.fr>
2023-10-04 11:35:51 +0200

    Added options to testing routine

--- trunk/examples/j77/bin/tests.sh
+++ trunk/examples/j77/bin/tests.sh
@@ -2,6 +2,15 @@
 
 exec_dir="`dirname $0`"
 
+diff_output="/tmp/syntax-diff.txt"
+
+if `test -n "$1" -a "$1" = "-v"`
+then
+    DETAILS="yes"
+else
+    DETAILS="no"
+fi    
+
 cd "${exec_dir}/.."
 
 for f in `ls test/nist/*.FOR`
@@ -10,5 +19,16 @@ do
 	expected="test-references/${test_file}.reference"
 	actual="test-references/${test_file}.actual"
 	bin/f77.out -json "${f}"  > "${actual}"
-	diff -q $expected $actual && rm $actual
+	if test $DETAILS = "yes"
+	then
+	    diff $expected $actual > $diff_output
+	    if test -s $diff_output
+	    then
+		less $diff_output
+	    else
+		rm $actual
+	    fi
+	else
+	    diff -q $expected $actual && rm $actual
+	fi
 done
