ed5552a12bf26826a423f26227dde4ee8545253d
anquetil <nicolas.anquetil@inria.fr>
2023-10-05 18:33:02 +0200

    new suite of smaller (unit) tests

--- trunk/examples/j77/bin/tests-oracle.sh
+++ trunk/examples/j77/bin/tests-oracle.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# create reference results (oracle) for the output of Syntax parser on
+# various example files
 
 exec_dir="`dirname $0`"
 
@@ -8,6 +10,13 @@ cd "${exec_dir}/.."
 rm test-references/*
 
 for f in `ls test/nist/*.FOR`
+do
+	test_file=`basename ${f}`
+	actual="test-references/${test_file}.reference"
+	bin/f77.out -json "${f}" | python -m json.tool > "${actual}"
+done
+
+for f in `ls test/unit-tests/*.FOR`
 do
 	test_file=`basename ${f}`
 	actual="test-references/${test_file}.reference"
--- trunk/examples/j77/bin/tests.sh
+++ trunk/examples/j77/bin/tests.sh
@@ -1,24 +1,45 @@
 #!/bin/sh
+# executes Syntax on a bunch of files and compare the result to a previously
+# recorded reference (see ./tests-oracle.sh)
+# Reports the files where some differences where found
+# 
+# OPTION:
+# "-v" shows the differences for each file one after the other (hit "q"to go to next file)
+# "-b" work on the big data set (test/nist files)
+#      default is to work on tests/unit-tests files
+
 
 exec_dir="`dirname $0`"
 
 diff_output="/tmp/syntax-diff.txt"
 
-if `test -n "$1" -a "$1" = "-v"`
-then
-    DETAILS="yes"
-else
-    DETAILS="no"
-fi    
+DETAILS="no"
+SUITE="unit-tests"
+
+# Loop through all the provided arguments
+for arg in "$@"
+do
+    # Check if arg1 is empty and assign the first argument to it
+    if [ "$arg" = "-v" ]
+    then
+        DETAILS="yes"
+    elif [ "$arg" = "-b" ]
+    then
+        SUITE="nist"
+    fi
+done
+
 
 cd "${exec_dir}/.."
 
-for f in `ls test/nist/*.FOR`
+for f in `ls test/${SUITE}/*.FOR`
 do	
 	test_file=`basename ${f}`
 	expected="test-references/${test_file}.reference"
 	actual="test-references/${test_file}.actual"
+
 	bin/f77.out -json "${f}" | python -m json.tool  > "${actual}"
+
 	if test $DETAILS = "yes"
 	then
 	    diff $expected $actual > $diff_output
@@ -29,6 +50,7 @@ do
 		rm $actual
 	    fi
 	else
+	    echo "diffing ${f}"
 	    diff -q $expected $actual && rm $actual
 	fi
 done
